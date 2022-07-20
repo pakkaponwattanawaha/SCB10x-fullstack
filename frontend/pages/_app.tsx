abfdffe88d54d7de7526019e1b937e93ddb8d5bf
pakkaponwattanawaha <macpakkapon@gmail.com>
2022-07-20 15:31:55 +0700

    feat: add redux slice and store

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -3,7 +3,8 @@ import { ReactNode } from "react";
 import { NextPage } from "next";
 import type { AppProps } from "next/app";
 import { Navbar } from "../components/Navbar";
-
+import { store } from "../store";
+import { Provider } from "react-redux";
 type NextPageWithLayout = NextPage & {
   getLayout?: () => ReactNode;
 };
@@ -15,10 +16,12 @@ type AppPropsWithLayout = AppProps & {
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page: any) => page);
   return (
-    <div className="h-screen mainBackground flex justify-center justify-items-center">
-      <Navbar />
-      {getLayout(<Component className="" {...pageProps} />)}
-    </div>
+    <Provider store={store}>
+      <div className="h-screen mainBackground flex justify-center justify-items-center">
+        <Navbar />
+        {getLayout(<Component className="" {...pageProps} />)}
+      </div>
+    </Provider>
   );
 }
 
---
+++ frontend/store/user/userSlice.ts
@@ -0,0 +1,71 @@
+import { createSlice, Draft, PayloadAction } from "@reduxjs/toolkit";
+
+export interface UserState {
+  id: string;
+  isLogin: boolean;
+  token: string;
+  email: string;
+}
+
+/**
+ * Default state object with initial values.
+ */
+const initialState: UserState = {
+  id: "",
+  isLogin: false,
+  token: "",
+  email: "",
+};
+
+/**
+ * Create a slice as a reducer containing actions.
+ *
+ * In this example actions are included in the slice. It is fine and can be
+ * changed based on your needs.
+ */
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    loginSuccess: (
+      state: Draft<UserState>,
+      action: PayloadAction<UserState>
+    ) => {
+      state.id = action.payload.id;
+      state.token = action.payload.token;
+      state.email = action.payload.email;
+      state.isLogin = action.payload.isLogin;
+    },
+    getUserDetails: (state: Draft<UserState>) => {
+      if (typeof window !== "undefined") {
+        state.token = localStorage.getItem("id");
+        state.token = localStorage.getItem("email");
+        state.token = localStorage.getItem("accessToken");
+      }
+    },
+
+    setUserDetails: (
+      state: Draft<UserState>,
+      action: PayloadAction<UserState>
+    ) => {
+      localStorage.setItem("id", action.payload.id);
+      localStorage.setItem("email", action.payload.email);
+      localStorage.setItem("accessToken", action.payload.token);
+    },
+
+    logout: (state: Draft<typeof initialState>) => {
+      localStorage.removeItem("id");
+      localStorage.removeItem("email");
+      localStorage.removeItem("accessToken");
+    },
+  },
+});
+
+// A small helper of user state for `useSelector` function.
+export const getUserState = (state: { user: UserState }) => state.user;
+
+// Exports all actions
+export const { loginSuccess, getUserDetails, setUserDetails, logout } =
+  userSlice.actions;
+
+export default userSlice.reducer;
