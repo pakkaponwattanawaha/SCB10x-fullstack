5b3d5cc07ce80343c3a27f1dea0a4cafe3898bcc
pakkaponwattanawaha <macpakkapon@gmail.com>
2022-07-20 17:31:01 +0700

    fix: handle component rerender loop

--- frontend/components/RequireAuth.tsx
+++ frontend/components/RequireAuth.tsx
@@ -1,13 +1,17 @@
 import Router from "next/router";
-import React from "react";
-
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 import { getUserDetails, getUserState } from "store/user/userSlice";
 const RequireAuth = ({ children }: { children: JSX.Element }) => {
   const dispatch = useDispatch();
-  dispatch(getUserDetails());
+
   const { token } = useSelector(getUserState);
+
+  useEffect(() => {
+    dispatch(getUserDetails());
+  }, []);
+
   if (token == null) {
     Router.push("/login");
   }
