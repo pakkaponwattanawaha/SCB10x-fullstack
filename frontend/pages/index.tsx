82644dbf706529e2794947b206559e1c21634533
pakkaponwattanawaha <macpakkapon@gmail.com>
2022-07-20 15:26:47 +0700

    feat: home page fetch ssr

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,27 +1,59 @@
+import axios from "axios";
+import { useDispatch, useSelector } from "../store";
+import { API_ENDPOINT } from "config";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import { useEffect } from "react";
+import { getUserState, loginSuccess } from "store/user/userSlice";
+import RequireAuth from "components/RequireAuth";
 
-const Home: NextPage = () => {
+export async function getServerSideProps(context) {
+  const response = await axios
+    .get(`http://127.0.0.1:3333/api/v1/party`, {
+      headers: {
+        Authorization:
+          "Bearer " +
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjJkNTE1ZjljOTdmZjA4OTE4NzdjYWUzIiwiZW1haWwiOiJhZG1pbjRAZ21haWwuY29tIn0sImlhdCI6MTY1ODIyNTY3MywiZXhwIjoxNjU4MzEyMDczfQ.f3-vEA0unkKQgvDSNWJE-0yaLGB0JfBSIpnZ_UTDtWY",
+      },
+    })
+    .then((response) => {
+      console.log("GET party", response);
+      console.log(response.data);
+      return response;
+    })
+    .catch((error) => {
+      console.log(error);
+      return error;
+    });
+  return {
+    props: {
+      parties: response.data,
+    },
+  };
+}
+
+const Home: NextPage = ({ parties }: any) => {
+  console.log(parties);
+
   return (
-    <div>Home</div>
-    // <div className="pt-[96px] h-screen mainBackground flex justify-center justify-items-center">
-    //   <div className="flex flex-col  items-center justify-items-center centered  rounded w-3/5  mt-10">
-    //     <h2 className="w-3/5 text-[42px] font-bold pb-3 text-white">
-    //       Leverage Lending{" "}
-    //     </h2>
-    //     <div className=" border w-3/5 bg-gray-50 justify-self-center shadow-xl rounded-xl p-5">
-    //       <div className="grid w-fit pt-3  rounded-lg  w-full px-16 py-3">
-    //         <button
-    //           type="submit"
-    //           className="w-full bg-main1 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-5 rounded-2xl focus:outline-none focus:shadow-outline"
-    //         >
-    //           Submit
-    //         </button>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
+    <RequireAuth>
+      <div className="pt-[96px]">
+        Home
+        {parties ? (
+          parties.map((party, idx) => {
+            return (
+              <div className="p-2 border-2" key={idx}>
+                {" "}
+                {JSON.stringify(party)}
+              </div>
+            );
+          })
+        ) : (
+          <></>
+        )}
+      </div>
+    </RequireAuth>
   );
 };
 
