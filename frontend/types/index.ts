1ef9c91727e3dbcdbe7f58c46c89b7b8753bc75e
pakkaponwattanawaha <macpakkapon@gmail.com>
2022-07-20 15:28:15 +0700

    feat: add data types

---
+++ frontend/types/index.ts
@@ -0,0 +1,22 @@
+export interface LoginFormDataType {
+  email: string;
+  password: string;
+}
+export interface RegisterFormDataType {
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
+export interface UserDetails {
+  id?: string;
+  email: string;
+}
+
+export class PartyDetails {
+  id?: string;
+  name: string;
+  description?: string;
+  owner: UserDetails;
+  limit: number;
+  members?: Array<UserDetails>;
+}
